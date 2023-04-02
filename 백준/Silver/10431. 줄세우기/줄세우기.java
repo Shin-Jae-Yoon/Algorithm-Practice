a07da2f4f7ccfa5b724b9464210b84d874e7d10f
Dev Uni <wlwhsvkdlxh@gmail.com>
2023-04-02 20:21:24 +0900

    [Silver V] Title: 줄세우기, Time: 180 ms, Memory: 14380 KB -BaekjoonHub

--- 백준/Silver/10431. 줄세우기/줄세우기.java
+++ 백준/Silver/10431. 줄세우기/줄세우기.java
@@ -5,33 +5,24 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int t = Integer.parseInt(br.readLine());
-        int[][] input = new int[t][21];
-        int[][] sorted = new int[t][21];
+        int[] input = new int[20];
 
-        int count = 0;
-        for (int i = 0; i < t; i++) {
+        for (int num = 0; num < t; num++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < 21; j++) {
-                input[i][j] = Integer.parseInt(st.nextToken());
+            st.nextToken();
+            for (int i = 0; i < 20; i++) {
+                input[i] = Integer.parseInt(st.nextToken());
             }
 
-            for (int j = 1; j < 21; j++) {
-                boolean find = false;
-                for (int k = 0; k < j; k++) {
-                    if (sorted[i][k] > input[i][j]) {
-                        find = true;
-                        for (int z = j - 1; z >= k; z--) {
-                            sorted[i][z + 1] = sorted[i][z];
-                            count++;
-                        }
-                        sorted[i][k] = input[i][j];
-                        break;
+            int count = 0;
+            for (int i = 0; i < 20; i++) {
+                for (int j = 0; j < i; j++) {
+                    if (input[j] > input[i]) {
+                        count++;
                     }
-                    if (!find) sorted[i][j] = input[i][j];
                 }
             }
-            System.out.println(i+1 + " " + count);
-            count = 0;
+            System.out.println((num + 1) + " " + count );
         }
     }
 }
