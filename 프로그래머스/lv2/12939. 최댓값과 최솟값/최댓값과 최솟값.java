30136b263d3d9cb8e1c96cecd8f06e247e393a1d
Dev Uni <wlwhsvkdlxh@gmail.com>
2023-04-06 22:41:36 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 8.39 ms, Memory: 74.5 MB -BaekjoonHub

--- 프로그래머스/lv2/12939. 최댓값과 최솟값/최댓값과 최솟값.java
+++ 프로그래머스/lv2/12939. 최댓값과 최솟값/최댓값과 최솟값.java
@@ -1,26 +1,14 @@
+import java.util.*;
 class Solution {
     public String solution(String s) {
         String[] input = s.split(" ");
-        int[] nums = new int[input.length];
-    
-        for (int i = 0; i < nums.length; i++) {
-            nums[i] = Integer.parseInt(input[i]);
+        ArrayList<Integer> arr = new ArrayList<Integer>();
+        
+        for (int i = 0; i < input.length; i++) {
+            arr.add(Integer.parseInt(input[i]));
         }
         
-        int max, min;
-        max = min = nums[0];
-        
-        for (int i = 0; i < nums.length; i++) {
-            if (max < nums[i]) {
-                max = nums[i];
-            }
-            
-            if (min > nums[i]) {
-                min = nums[i];
-            }
-        }
-        
-        String answer = min + " " + max;
+        String answer = Collections.min(arr) + " " + Collections.max(arr);
         return answer;
     }
 }
