39e3c791ad6ac063a9494d1f4b7146e6d3aea44c
Dev Uni <wlwhsvkdlxh@gmail.com>
2023-04-10 23:14:34 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.02 ms, Memory: 80.6 MB -BaekjoonHub

--- 프로그래머스/lv1/12931. 자릿수 더하기/자릿수 더하기.java
+++ 프로그래머스/lv1/12931. 자릿수 더하기/자릿수 더하기.java
@@ -2,14 +2,13 @@ import java.util.*;
 
 public class Solution {
     public int solution(int n) {
-        String input = String.valueOf(n);
-        char[] input2 = input.toCharArray();
         int sum = 0;
-        
-        for (int i = 0; i < input2.length; i++) {
-            sum += (int)input2[i] - '0';
+        while (true) {
+            sum += n % 10;
+            if (n < 10)
+                break;
+            n /= 10;
         }
-
         return sum;
     }
 }
