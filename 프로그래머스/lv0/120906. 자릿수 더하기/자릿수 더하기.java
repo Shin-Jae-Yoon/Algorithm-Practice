b3907ed990808b789af07208a29491556497a6b4
Dev Uni <wlwhsvkdlxh@gmail.com>
2023-04-15 19:43:06 +0900

    [level 0] Title: 자릿수 더하기, Time: 0.05 ms, Memory: 65.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120906. 자릿수 더하기/자릿수 더하기.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int solution(int n) {
+        String s = String.valueOf(n);
+        int answer = 0;
+        
+        for (int i = 0; i < s.length(); i++) {
+            answer += Integer.parseInt(String.valueOf(s.charAt(i)));
+        }
+        
+        return answer;
+    }
+}
