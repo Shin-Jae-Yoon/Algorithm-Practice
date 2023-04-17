5ca73d2c6514d33718dcab3b53d52ecfc5b799d8
Dev Uni <wlwhsvkdlxh@gmail.com>
2023-04-17 18:45:42 +0900

    [level 0] Title: 외계행성의 나이, Time: 1.67 ms, Memory: 73 MB -BaekjoonHub

--- 프로그래머스/lv0/120834. 외계행성의 나이/외계행성의 나이.java
+++ 프로그래머스/lv0/120834. 외계행성의 나이/외계행성의 나이.java
@@ -1,25 +1,13 @@
-import java.util.HashMap;
 class Solution {
     public String solution(int age) {
-        HashMap<Integer, String> map = new HashMap<>();
-        map.put(0, "a");
-        map.put(1, "b");
-        map.put(2, "c");
-        map.put(3, "d");
-        map.put(4, "e");
-        map.put(5, "f");
-        map.put(6, "g");
-        map.put(7, "h");
-        map.put(8, "i");
-        map.put(9, "j");
-        
         String answer = "";
-        String[] arrAge = String.valueOf(age).split("");
-        
-        for (int i = 0; i < arrAge.length; i++) {
-            answer += map.get(Integer.parseInt(arrAge[i]));
+        String[] alpha = new String[]{"a","b","c","d","e","f","g","h","i","j"};
+
+        while(age>0){
+            answer = alpha[age % 10] + answer;
+            age /= 10;
         }
-        
+
         return answer;
     }
 }
