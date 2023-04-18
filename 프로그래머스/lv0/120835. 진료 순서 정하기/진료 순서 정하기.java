46570c80e2bb88cc553b106776594ee8fddf896c
Dev Uni <wlwhsvkdlxh@gmail.com>
2023-04-18 22:57:38 +0900

    [level 0] Title: 진료 순서 정하기, Time: 0.56 ms, Memory: 76.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120835. 진료 순서 정하기/진료 순서 정하기.java
+++ 프로그래머스/lv0/120835. 진료 순서 정하기/진료 순서 정하기.java
@@ -1,23 +1,21 @@
+import java.util.HashMap;
+import java.util.Arrays;
+
 class Solution {
     public int[] solution(int[] emergency) {
         int[] answer = new int[emergency.length];
+        HashMap<Integer, Integer> map = new HashMap<>();
 
-        for(int i = 0; i < answer.length; i++){
-            
-            if(answer[i] != 0){
-                continue;
-            }
-            
-            int idx = 1;
-            
-            for(int j = 0; j < answer.length; j++){
-                if(emergency[i] < emergency[j]){
-                    idx++;
-                }
-            }
-            
-            answer[i] = idx;
+        for (int i = 0; i < emergency.length; i++) {
+            map.put(emergency[i], i);
         }
+
+        Arrays.sort(emergency);
+        
+        for (int i = emergency.length - 1; i >= 0; i--) {
+            answer[map.get(emergency[i])] = emergency.length - i;
+        }
+        
         return answer;
     }
 }
