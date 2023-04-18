4286d6e460508ab0700669bf33c8c43fe2dfa97c
Dev Uni <wlwhsvkdlxh@gmail.com>
2023-04-18 16:34:32 +0900

    [level 0] Title: 숫자 찾기, Time: 0.04 ms, Memory: 65.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120904. 숫자 찾기/숫자 찾기.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int solution(int num, int k) {
+        
+        String str = String.valueOf(num);
+        int answer = -1;
+        
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) - '0' == k) {
+                answer = i + 1;
+                break;
+            }
+        }
+
+        return answer;
+    }
+}
