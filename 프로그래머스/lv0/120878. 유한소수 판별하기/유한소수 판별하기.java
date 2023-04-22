f430132f7f920415b0f4f343383b1d155d3813f7
Dev Uni <wlwhsvkdlxh@gmail.com>
2023-04-22 20:17:59 +0900

    [level 0] Title: 유한소수 판별하기, Time: 0.03 ms, Memory: 79.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120878. 유한소수 판별하기/유한소수 판별하기.java
+++ 프로그래머스/lv0/120878. 유한소수 판별하기/유한소수 판별하기.java
@@ -1,42 +1,20 @@
 class Solution {
     public int solution(int a, int b) {
-        int answer = 2;
-        int up = a;
-        int down = b;
-        
-        // 분자가 분모보다 크면 나눠주기
-        if (up >= down) {
-            if (up % down == 0) {
-                answer = 1;
-            } else {
-                up = up % down;
-            }
+        int answer = 0;
+
+        while (b % 2 == 0) {
+            b /= 2;
         }
         
-        // 약분 가능하면 약분해주기
-        for (int i = 2; i <= down; i++) {
-            if (up % i == 0 && down % i ==0) {
-                up /= i;
-                down /= i;
-            }
+        while (b % 5 == 0) {
+            b /= 5;
         }
         
-        while (down > 0) {
-            if (down % 5 == 0)
-                down /= 5;
-            
-            if (down % 2 == 0)
-                down /= 2;
-            
-            if (down == 1) {
-                answer = 1;
-                break;
-            } else if (down != 1 && down % 2 != 0 && down % 5 != 0) {
-                answer = 2;
-                break;
-            }
-        }
-           
+        if ((a / (double)b) % 1 == 0)
+            answer = 1;
+        else
+            answer = 2;
+
         return answer;
     }
 }
