1865b80e9b67deb15e7f14258c0463dd00f14242
Dev Uni <wlwhsvkdlxh@gmail.com>
2023-04-23 16:23:49 +0900

    [level 0] Title: 다음에 올 숫자, Time: 0.02 ms, Memory: 75.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120924. 다음에 올 숫자/다음에 올 숫자.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int solution(int[] common) {
+        int answer = 0;
+        
+        int distance1 = common[1] - common[0];
+        int distance2 = common[2] - common[1];
+        
+        if (distance1 == distance2) {
+            answer = common[common.length - 1] + distance1;
+        } else {
+            answer = common[common.length - 1] * (common[1] / common[0]);
+        }
+        
+        if (common[0] == 1 && common[1] == 1)
+            answer = 1;
+        
+        return answer;
+    }
+}
