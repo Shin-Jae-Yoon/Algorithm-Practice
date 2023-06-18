86ebb5f445e7379958ff20e1c95b2f597f715c8e
Dev Uni <wlwhsvkdlxh@gmail.com>
2023-06-18 21:46:16 +0900

    [level 2] Title: 중성화 여부 파악하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv2/59409. 중성화 여부 파악하기/중성화 여부 파악하기.sql
+++ 프로그래머스/lv2/59409. 중성화 여부 파악하기/중성화 여부 파악하기.sql
@@ -1,6 +1,7 @@
 select animal_id, name, 
-    case when sex_upon_intake like 'Intact%' then 'X'
-                         else 'O'
-                         end as 중성화
+    case 
+        when sex_upon_intake like 'Intact%' then 'X'
+        else 'O'
+        end as 중성화
 from animal_ins
 order by animal_id;
