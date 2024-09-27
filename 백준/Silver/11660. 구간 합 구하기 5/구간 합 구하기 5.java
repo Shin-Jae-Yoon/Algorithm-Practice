2598b57331891c983d93117cf5ec36faae7913f5
Dev Uni <wlwhsvkdlxh@gmail.com>
2024-09-27 21:43:41 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 732 ms, Memory: 125736 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
@@ -4,12 +4,6 @@ import java.io.IOException;
 import java.lang.StringBuilder;
 import java.util.StringTokenizer;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.IOException;
-import java.lang.StringBuilder;
-import java.util.StringTokenizer;
-
 public class Main {
     public static void main(String[] args) throws IOException {
         StringBuilder sb = new StringBuilder();
