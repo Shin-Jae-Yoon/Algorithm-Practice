06d031baca605f26a23eea2a70a1ed0d8cd08b6c
Dev Uni <wlwhsvkdlxh@gmail.com>
2024-10-03 17:22:54 +0900

    [Silver II] Title: 스택 수열, Time: 304 ms, Memory: 27248 KB -BaekjoonHub

--- 백준/Silver/1874. 스택 수열/스택 수열.java
+++ 백준/Silver/1874. 스택 수열/스택 수열.java
@@ -11,34 +11,40 @@ public class Main {
         int n = Integer.parseInt(br.readLine());
 
         Stack<Integer> stack = new Stack<>();
+        boolean isPossible = true;
         int current = 1;
 
         while (n > 0) {
             int target = Integer.parseInt(br.readLine());
 
-            while (current <= target) {
-                 if (stack.isEmpty() || stack.peek() != target) {
+            if (current <= target) {
+                while (current <= target) {
                     stack.push(current);
                     sb.append("+");
                     sb.append("\n");
                     current++;
                 }
-            }
 
-            if (!stack.isEmpty() && stack.peek() == target) {
                 stack.pop();
                 sb.append("-");
                 sb.append("\n");
+            } else {
+                if (stack.pop() > target) {
+                    isPossible = false;
+                    break;
+                } else {
+                    sb.append("-");
+                    sb.append("\n");
+                }
             }
 
             n--;
         }
 
-        if (!stack.isEmpty()) {
+        if (isPossible) {
+            System.out.println(sb.toString());
+        } else {
             System.out.println("NO");
-            return;
         }
-
-        System.out.println(sb.toString());
     }
 }
