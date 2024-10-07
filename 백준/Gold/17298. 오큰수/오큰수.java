11e53ac7a58d3a68d7def415bae16ee6ef5e3e16
Dev Uni <wlwhsvkdlxh@gmail.com>
2024-10-08 01:20:51 +0900

    [Gold IV] Title: 오큰수, Time: 868 ms, Memory: 148336 KB -BaekjoonHub

--- 백준/Gold/17298. 오큰수/오큰수.java
+++ 백준/Gold/17298. 오큰수/오큰수.java
@@ -2,47 +2,39 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.lang.StringBuilder;
-import java.util.*;
+import java.util.Arrays;
+import java.util.Stack;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         StringBuilder sb = new StringBuilder();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        int[] _list = new int[n];
+        int[] numbers = new int[n];
+        int[] answer = new int[n];
 
         StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 0; i < n; i++) {
-            _list[i] = Integer.parseInt(st.nextToken());
+            numbers[i] = Integer.parseInt(st.nextToken());
         }
 
         Stack<Integer> stack = new Stack<>();
-        Deque<Integer> answer = new ArrayDeque<>();
+        Arrays.fill(answer, -1);
 
-        for (int i = n - 1; i >= 0; i--) {
-            while (true) {
-                if (stack.isEmpty()) {
-                    answer.addFirst(-1);
-                    stack.push(_list[i]);
-                    break;
-                }
-                int num = stack.pop();
-                if (num > _list[i]) {
-                    answer.addFirst(num);
-                    stack.push(num);
-                    stack.push(_list[i]);
-                    break;
-                }
+        for (int i = 0; i < numbers.length; i++) {
+            while (!stack.isEmpty() && numbers[i] > numbers[stack.peek()]) {
+                answer[stack.pop()] = numbers[i];
             }
+            
+            stack.push(i);
         }
 
-        // 출력
-        for (int num : answer) {
-            sb.append(num);
+        for (int i = 0; i < answer.length; i++) {
+            sb.append(answer[i]);
             sb.append(" ");
         }
 
         System.out.println(sb.toString());
     }
 }
-
