bfd38fbde5aa5dc485fdee296e99b3c5b5492ff0
Dev Uni <wlwhsvkdlxh@gmail.com>
2025-09-13 23:25:27 +0900

    [Bronze II] Title: 피보나치 수 5, Time: 100 ms, Memory: 14160 KB -BaekjoonHub

---
+++ 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        System.out.println(fiboRecursion(n));
+    }
+
+    private static int fiboRecursion(int n) {
+        if (n == 0) {
+            return 0;
+        }
+
+        if (n == 1) {
+            return 1;
+        }
+
+        return fiboRecursion(n - 1) + fiboRecursion(n - 2);
+    }
+}
